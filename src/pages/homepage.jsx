646d70d171ab13a49482de10f76e772cbeefa3d2
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-03-17 17:50:14 +0100

    map and axios for film

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,3 +1,46 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 export default function Homepage() {
-  return <h1 className="mt-4">Home</h1>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/movies')
+      .then((res) => {
+        setMovies(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1 className="mt-4">Lista Film</h1>
+
+      <div className="row">
+        {movies.map((movie) => {
+          return (
+            <div className="col-md-4 mb-4">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src={`http://localhost:3000/img/movies_cover/${movie.image}`}
+                  alt={movie.title}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{movie.title}</h5>
+                  <Link to={`/movies/${movie.id}`} className="btn btn-primary">
+                    Dettaglio
+                  </Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
