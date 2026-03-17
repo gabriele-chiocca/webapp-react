480376711066ba7debacb76a4c64a8ed12b2f668
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-03-17 17:08:46 +0100

    creating default layout after setup on routes, and route

---
+++ src/components/DefaultLayout.jsx
@@ -0,0 +1,47 @@
+import { Outlet } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Homepage from '../pages/homepage';
+
+export default function DefaultLayout() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink to={Homepage} className="navbar-brand">
+          Navbar
+        </NavLink>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <NavLink to={Homepage} className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <main>
+        <div className="container">
+          <Outlet></Outlet>
+        </div>
+      </main>
+    </>
+  );
+}
